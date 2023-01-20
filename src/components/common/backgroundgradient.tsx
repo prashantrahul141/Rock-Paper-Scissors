546deeb5f8ab8b747e7fc544eb7e3636a4bc9a19
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-21 01:41:25 +0530

    changing gradient size

--- src/components/common/backgroundgradient.tsx
+++ src/components/common/backgroundgradient.tsx
@@ -3,7 +3,7 @@ import type { FC } from 'react';
 const BackgroundGradient: FC = () => {
   return (
     <div className='fixed -z-50 h-screen w-screen bg-themePrimary-1300'>
-      <div className='fixed -top-[13rem] left-1/2 -z-40 h-[32rem] w-[32rem] -translate-x-1/2 rounded-full bg-gradient-radial from-themePrimary-500 to-themePrimary-1000 opacity-20 blur-[120px]'></div>
+      <div className='fixed -top-[32rem] left-1/2 -z-40 h-[64rem] w-[64rem] -translate-x-1/2 rounded-full bg-gradient-radial from-themePrimary-500 to-themePrimary-1000 opacity-20 blur-[120px]'></div>
     </div>
   );
 };
