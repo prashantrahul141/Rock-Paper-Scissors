2daffa7cb10b3a7e1dd438094aa57485ce35ebb5
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-20 22:47:33 +0530

    adding component

---
+++ src/components/room/score.tsx
@@ -0,0 +1,18 @@
+import type { FC } from 'react';
+
+const Score: FC<{ score: number }> = ({ score }) => {
+  return (
+    <div className='w-max rounded-md border border-themePrimary-300 bg-themePrimary-50/5 py-3 px-4 text-center'>
+      <div>
+        <span className='font-inconsolata text-themePrimary-50'>Score</span>
+      </div>
+      <div>
+        <span className='font-secularOne text-6xl text-themePrimary-400'>
+          {score}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Score;
