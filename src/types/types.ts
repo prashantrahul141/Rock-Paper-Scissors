ab334e2c65d73240f117ee17402ca36c716daafc
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-20 23:28:53 +0530

    adding TypeStatusText

---
+++ src/types/types.ts
@@ -0,0 +1,7 @@
+enum TypeStatusText {
+  'WAITING_FOR_PLAYER' = 'Waiting for another player to join...',
+  'WAITING_FOR_OTHER_PLAYER_MOVE' = 'Waiting for other player to pick',
+  'WAITING_FOR_YOUR_PICK' = 'Pick one!',
+}
+
+export default TypeStatusText;
