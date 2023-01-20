58d58821860eaf0ce2de47b4e7edd370cdeaa167
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-20 18:44:05 +0530

    adding welcome component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import HeadComp from '@components/common/headcomponent';
+import Welcome from '@components/home/welcome';
 import { type NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
     <>
       <HeadComp></HeadComp>
+
+      <Welcome></Welcome>
     </>
   );
 };
