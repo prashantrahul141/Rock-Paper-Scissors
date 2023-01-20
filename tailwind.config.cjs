a80b5035afaea3d00635f5523a8e773969eb3004
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-20 17:46:45 +0530

    adding background gradient

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
       fontFamily: {
         bebasNeue: ['Bebas Neue'],
         inconsolata: ['Inconsolata monospace'],
