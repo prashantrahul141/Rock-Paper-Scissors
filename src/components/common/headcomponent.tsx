7f51fa16c7126e4a6dd1a3e6e3a33c281a428ab5
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-20 18:44:30 +0530

    removing uncessary jsx

--- src/components/common/headcomponent.tsx
+++ src/components/common/headcomponent.tsx
@@ -6,27 +6,21 @@ const HeadComp: FC<{ headTitle?: string | undefined }> = ({
 }) => {
   // Head component to define meta tags
   return (
-    <>
-      <Head>
-        {headTitle === undefined && <title>{`Play Rock Paper Scissors`}</title>}
-        {headTitle !== undefined && <title>{`${headTitle}`}</title>}
-        <meta
-          name='description'
-          charSet='UTF-8'
-          content='Rock Paper Scissors'
-        />
-        {/* <link rel='manifest' href='/manifest.json'></link> */}
-        <meta name='darkreader-lock' />
-        <meta content='Rock Paper Scissors' property='og:title' />
-        <meta content='Play Rock Paper Scissors!' property='og:description' />
-        <meta
-          content='https://rock-paper-scissors.up.railway.app'
-          property='og:url'
-        />
-        <meta content='' property='og:image' />
-        <meta content='#448fef' data-react-helmet='true' name='theme-color' />
-      </Head>
-    </>
+    <Head>
+      {headTitle === undefined && <title>{`Play Rock Paper Scissors`}</title>}
+      {headTitle !== undefined && <title>{`${headTitle}`}</title>}
+      <meta name='description' charSet='UTF-8' content='Rock Paper Scissors' />
+      {/* <link rel='manifest' href='/manifest.json'></link> */}
+      <meta name='darkreader-lock' />
+      <meta content='Rock Paper Scissors' property='og:title' />
+      <meta content='Play Rock Paper Scissors!' property='og:description' />
+      <meta
+        content='https://rock-paper-scissors.up.railway.app'
+        property='og:url'
+      />
+      <meta content='' property='og:image' />
+      <meta content='#448fef' data-react-helmet='true' name='theme-color' />
+    </Head>
   );
 };
 
