3b68aa252a20201776ff2423c9a317fb45d66175
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-20 18:44:12 +0530

    styling changes

--- src/components/common/backgroundgradient.tsx
+++ src/components/common/backgroundgradient.tsx
@@ -2,10 +2,8 @@ import type { FC } from 'react';
 
 const BackgroundGradient: FC = () => {
   return (
-    <div className='h-screen w-screen'>
-      <div className='h-full w-full bg-themePrimary-1300'>
-        <div className='absolute -top-[13rem] left-1/2 h-[32rem] w-[32rem] -translate-x-1/2 rounded-full bg-gradient-radial from-themePrimary-500 to-themePrimary-1000 opacity-20 blur-[120px]'></div>
-      </div>
+    <div className='fixed -z-50 h-screen w-screen bg-themePrimary-1300'>
+      <div className='fixed -top-[13rem] left-1/2 -z-40 h-[32rem] w-[32rem] -translate-x-1/2 rounded-full bg-gradient-radial from-themePrimary-500 to-themePrimary-1000 opacity-20 blur-[120px]'></div>
     </div>
   );
 };
