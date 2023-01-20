1f7b04ffc95aaba6565438371e7721eaa9347025
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-20 20:23:54 +0530

    state for button disabling

--- src/components/home/welcomeinput.tsx
+++ src/components/home/welcomeinput.tsx
@@ -1,11 +1,31 @@
-import type { FC } from 'react';
+import type { ChangeEvent, FC } from 'react';
+import { useState } from 'react';
 
 const WelcomeInput: FC = () => {
+  const [disableButton, setDisableButton] = useState(true);
+  const [inputValue, setInputValue] = useState('');
+
+  const inputHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const trimmedValue = e.target.value.trim();
+    setInputValue(trimmedValue);
+    trimmedValue.length === 6
+      ? setDisableButton(false)
+      : setDisableButton(true);
+  };
+
   return (
     <div className='flex gap-4'>
-      <input className='input flex-grow' placeholder='Room number'></input>
+      <input
+        min={5}
+        max={6}
+        type={'text'}
+        onChange={inputHandler}
+        className='input flex-grow'
+        placeholder='Room number'></input>
       <div className='flex items-center justify-center'>
-        <button className='btn h-fit w-fit'>Join!</button>
+        <button disabled={disableButton} className='btn h-fit w-fit'>
+          Join!
+        </button>
       </div>
     </div>
   );
