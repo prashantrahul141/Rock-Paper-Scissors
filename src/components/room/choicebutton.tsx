a63e6940b3347c5fb4a5a829035695b71881e07e
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-21 15:40:48 +0530

    spring type and duration

--- src/components/room/choicebutton.tsx
+++ src/components/room/choicebutton.tsx
@@ -10,11 +10,12 @@ const ChoiceButton: FC<{ color: string; icon: string; disabled: boolean }> = ({
     <motion.div
       whileHover={{ scale: 1.2, rotate: 360 }}
       whileTap={{ scale: 1.09 }}
-      className={`relative cursor-pointer rounded-full border-8 bg-themePrimary-50/95 transition-all duration-75 ease-in-out  ${color} p-12 ${
+      transition={{ type: 'spring', duration: 1 }}
+      className={`relative cursor-pointer rounded-full border-8 bg-themePrimary-50/95 ${color} p-12 ${
         disabled ? 'opacity-60' : 'opacity-100'
       }`}>
       <div className='absolute transition-all duration-200 ease-in-out'>
-        <span className='absolute -translate-x-1/2 -translate-y-1/2'>
+        <span className='absolute -translate-x-1/2 -translate-y-1/2 text-3xl'>
           {icon}
         </span>
       </div>
