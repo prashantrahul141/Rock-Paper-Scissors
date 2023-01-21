c2d38c53ea5b222408c22f87dfbe961ff6417834
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-21 15:56:38 +0530

    adding call back function

--- src/components/home/welcome.tsx
+++ src/components/home/welcome.tsx
@@ -2,8 +2,18 @@ import type { FC } from 'react';
 import WelcomeTitle from './welcometitle';
 import WelcomeInput from './welcomeinput';
 import WelcomeButtons from './welcomebuttons';
+import { api } from '@utils/api';
+import { useRouter } from 'next/router';
 
 const Welcome: FC = () => {
+  const createRoomMutation = api.mainRouter.createRoom.useMutation();
+  const router = useRouter();
+
+  const createRoomCallback = async () => {
+    const createdRoom = await createRoomMutation.mutateAsync();
+    void router.push(`/${createdRoom.roomId}`);
+  };
+
   return (
     <div className='absolute flex h-screen w-screen items-center justify-center'>
       <div className='h-max w-96 max-w-full'>
@@ -14,7 +24,7 @@ const Welcome: FC = () => {
         <WelcomeInput></WelcomeInput>
 
         {/* bottom buttons */}
-        <WelcomeButtons></WelcomeButtons>
+        <WelcomeButtons createCallback={createRoomCallback}></WelcomeButtons>
       </div>
     </div>
   );
--- src/components/home/welcomebuttons.tsx
+++ src/components/home/welcomebuttons.tsx
@@ -1,10 +1,18 @@
 import Link from 'next/link';
 import type { FC } from 'react';
 
-const WelcomeButtons: FC = () => {
+const WelcomeButtons: FC<{ createCallback: () => Promise<void> }> = ({
+  createCallback,
+}) => {
   return (
     <div className='mt-10 flex items-center justify-center gap-4'>
-      <button className='btn w-fit'>Create room!</button>
+      <button
+        onClick={async () => {
+          await createCallback();
+        }}
+        className='btn w-fit'>
+        Create room!
+      </button>
       <Link href={'/rules'}>
         <button className='btn flex w-fit border border-themePrimary-300 bg-transparent hover:bg-themePrimary-200/20'>
           Rules
