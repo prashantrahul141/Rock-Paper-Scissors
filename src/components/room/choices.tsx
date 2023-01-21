88cd6b331256605d698825658b74b88a291e1d5e
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-01-21 15:41:05 +0530

    moving rock button above

--- src/components/room/choices.tsx
+++ src/components/room/choices.tsx
@@ -3,23 +3,23 @@ import ChoiceButton from './choicebutton';
 
 const Choices: FC<{ disabled: boolean }> = ({ disabled }) => {
   const choiceOptions = {
-    rock: { color: 'border-red-400', icon: '@' },
-    paper: { color: 'border-blue-400', icon: '#' },
-    scissor: { color: 'border-yellow-400', icon: '$' },
+    rock: { color: 'border-red-400', icon: '✊' },
+    paper: { color: 'border-blue-400', icon: '👋' },
+    scissor: { color: 'border-yellow-400', icon: '✌️' },
   };
 
   return (
     <div className='relative mt-40 h-60 w-full max-w-sm sm:mt-28'>
-      <div className='absolute left-2'>
+      <div className='absolute left-2 rotate-45'>
         <ChoiceButton
-          color={choiceOptions.paper.color}
-          icon={choiceOptions.paper.icon}
+          color={choiceOptions.rock.color}
+          icon={choiceOptions.rock.icon}
           disabled={disabled}></ChoiceButton>
       </div>
       <div className='absolute right-2'>
         <ChoiceButton
-          color={choiceOptions.rock.color}
-          icon={choiceOptions.rock.icon}
+          color={choiceOptions.paper.color}
+          icon={choiceOptions.paper.icon}
           disabled={disabled}></ChoiceButton>
       </div>
       <div className='absolute bottom-0 left-1/2 -translate-x-1/2'>
